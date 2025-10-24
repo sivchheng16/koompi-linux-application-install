7dd134f1a18e6977f16f5d5a25ffa9c21f0087d0
sivchheng16 <kheangsivechheng@gmail.com>
2025-10-24 10:23:33 +0700

    Script-Application

---
+++ koompi-tmp-install.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+
+# Check for directory only
+if [ -d "$HOME/koompi-tmp-install" ]; then
+    rm -rf "$HOME/koompi-tmp-install"
+fi
+
+if [ ! -e "$HOME/koompi-tmp-install" ]; then
+    # File or folder does not exist, perform an action
+    echo "File or folder does not exist."
+fi
+
+
+
+# Install Rust 
+echo "Next, install Rust, first"
+
+
+#Check if Rush installed
+
+if ! command -v rustc &> /dev/null; then
+    echo "Rust is not installed. Installing now..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    . "$HOME/.cargo/env" 
+else
+    echo "Rust is already installed."
+fi
+
+
+sleep 5
+
+# Install zsh and oh-my-zsh
+
+# Check if Zsh is installed
+if ! command -v zsh &> /dev/null; then
+    echo "Zsh is not installed. Installing now..."
+    sudo pacman -Sy zsh
+else 
+    echo "zsh is already installed."
+fi
+
+
+sleep 5
+# Check if Oh My Zsh is installed
+if [ ! -d "$ZSH" ]; then
+    echo "Oh My Zsh is not installed. Installing now..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    
+    source ~/.zshrc
+else 
+    echo "oh-my-zsh is already installed"
+fi
+
+
+sleep 5code
+#Install zip
+if ! command -v zip &> /dev/null; then
+    echo "zip is not installed. Let's Install..."
+    sudo pacman -Sy zip
+else
+    echo "Zip is already installed."
+fi
+
+sleep 5
+
+
+#Install Telegram
+if ! command -v telegram-desktop &> /dev/null; then
+    echo "Telegram is not installed. Installing now..."
+    sudo pacman -Sy telegram-desktop
+else
+    echo "Telegram is already installed."
+fi
+
+
+sleep 10
+
+#Install libroffice
+if ! command -v libreoffice &> /dev/null; then
+    echo "LibreOffice is not installed. Installing now..."
+    sudo pacman -Sy --noconfirm libreoffice-fresh
+else
+    echo "LibreOffice is already installed."
+fi
+
+
+sleep 5
+
+# Install koompi-cli from Github
+mkdir $HOME/koompi-tmp-install
+
+cd koompi-tmp-install
+
+
+git clone https://github.com/koompi/koompi-cli.git
+
+cd $HOME/koompi-tmp-install/koompi-cli
+
+cargo build --release
+
+sleep 5
+
+sudo cp target/release/koompi /usr/local/bin
+
+cd $HOME
+
+
+sleep 10
+#Install visual studio code
+if ! command -v code &> /dev/null; then
+    echo "Visual Studio Code is not installed. Installing now..."
+    koompi install visual-studio-code-bin
+
+else
+    echo "Visual Studio Code is already installed."
+fi
+
+echo "Success Install, Let's Test"
+
+sleep 5
+
+echo "Successfull installed"
+
+
+
